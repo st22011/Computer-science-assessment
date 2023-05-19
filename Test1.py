532910511e242480cb129ee4f3923bdc9a962766
st22011 <132409946+st22011@users.noreply.github.com>
2023-05-19 12:53:19 +1200

    Update Test1.py
    Made it so that you can only input letters and spaces in the customer name widget
    Added a minimum quantity of 1 and a maximum of 500

--- Test1.py
+++ Test1.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 class PartyHireApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Party Hire Service")
 
-        self.items = ["Tables", "Chairs", "Decorations", "Sound System","BBQ",]  # List of available items
+        self.items = ["Tables", "Chairs", "Decorations", "Sound System", "BBQ"]  # List of available items
         self.orders = []  # List to store orders
 
         self.create_widgets()
@@ -30,6 +31,12 @@ class PartyHireApp:
         self.dropdown_item = tk.OptionMenu(input_frame, self.selected_item, *self.items)
         self.dropdown_item.grid(row=1, column=1)
 
+        # Quantity
+        lbl_quantity = tk.Label(input_frame, text="Quantity:")
+        lbl_quantity.grid(row=2, column=0, sticky="w")
+        self.entry_quantity = tk.Entry(input_frame)
+        self.entry_quantity.grid(row=2, column=1)
+
         # Order Button
         self.btn_order = tk.Button(
             self.root, text="Place Order", command=self.place_order, width=20
@@ -48,34 +55,46 @@ class PartyHireApp:
 
         # Validate Customer Name Entry
         self.entry_name.config(validate="key", validatecommand=(self.root.register(self.validate_customer_name), "%P"))
+        self.entry_quantity.config(validate="key", validatecommand=(self.root.register(self.validate_quantity), "%P"))
 
     def validate_customer_name(self, input_text):
         # Validate customer name input
-        if input_text.isalpha() or input_text == "":
+        if all(char.isalpha() or char.isspace() for char in input_text):
             return True
         else:
-            messagebox.showerror("Error", "Customer name can only contain alphabetic characters.")
+            messagebox.showerror("Error", "Customer name can only contain alphabetic characters and spaces.")
+            return False
+
+    def validate_quantity(self, input_text):
+        # Validate quantity input
+        if input_text.isdigit() and 1 <= int(input_text) <= 500 or input_text == "":
+            return True
+        else:
+            messagebox.showerror("Error", "Quantity must be a number between 1 and 500.")
             return False
 
     def place_order(self):
         # Get input values
         name = self.entry_name.get()
         item = self.selected_item.get()
+        quantity = self.entry_quantity.get()
 
         # Validate input
-        if not name or not item:
+        if not name or not item or not quantity:
             messagebox.showerror("Error", "Please fill in all fields.")
             return
 
         # Add order to the list
-        order = f"Customer: {name} - Item: {item}"
+        order = f"Customer: {name} - Item: {item} - Quantity: {quantity}"
         self.orders.append(order)
 
         # Update listbox
+        self.listbox_orders.insert(tk.END,)
         self.listbox_orders.insert(tk.END, order)
 
         # Clear input fields
         self.entry_name.delete(0, tk.END)
+        self.entry_quantity.delete(0, tk.END)
 
     def delete_order(self):
         # Get the selected index
@@ -84,9 +103,6 @@ class PartyHireApp:
         if selected_index:
             # Remove order from the list
             self.orders.pop(selected_index[0])
-
-            # Delete the selected item from the listbox
-            self.listbox_orders.delete(selected_index)
 
 
 # Create the main window
